4e90213ac921661770252fcbcf5b61cbe1ca1c09
thangnguyenvn98 <thangnguyenvn647@gmail.com>
2023-04-02 17:07:00 -0400

    Fixing Type Error second try

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,15 +1,17 @@
 import { initMongoose } from '@/lib/mongoose';
 import Product from '@/models/Product';
+import { NextApiRequest, NextApiResponse } from 'next';
+
 
 export async function findAllProducts(){
     return Product.find().exec()
 }
 
-export default async function handle(req,res){
+export default async function handle(req: NextApiRequest, res: NextApiResponse){
     await initMongoose()
     const {ids}= req.query
     if(ids){
-        const idsArray = ids.split(",") //id array method to add unique id items to an array and find it then return
+        const idsArray = Array.isArray(ids) ? ids : ids.split(",") //id array method to add unique id items to an array and find it then return
         res.json(await Product.find({'_id':{$in:idsArray}}).exec()) //$in: meaning find _id in the array
     }else{res.json( await findAllProducts())}
     
